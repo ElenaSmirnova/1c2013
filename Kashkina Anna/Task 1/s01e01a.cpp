b9937bfe33e711ad18f19b1995b36b89b0721265
C0CDDF@gmail.com <C0CDDF@gmail.com@0f01eb16-6d9f-4518-cd9d-0c10718a35a1>
2013-12-24 07:58:41 +0000

    -

--- Kashkina Anna/Task 1/s01e01a.cpp
+++ Kashkina Anna/Task 1/s01e01a.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 using namespace std;
 
-//Fibonacci : iterative
+//Fibonacci : iterative, modulo 2^64
 
 const int maxn = 200000;
-int fib[maxn];
+unsigned long long fib[maxn];
+
+int check(int n) {
+    if (n >= 0 && n < maxn) return 1;
+    cout << "Ivalid input\n";
+    exit(0);
+}
 
 int main() {
     int n;
     fib[0] = 0;
     fib[1] = 1;
     cin >> n;
+    check(n);
     for (int i = 2; i <= n; ++i)
         fib[i] = fib[i - 1] + fib[i - 2];
     cout << fib[n] << endl;
