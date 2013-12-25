448cde69d52af1e8f7c9b87e1f6b8a68cfeef7dc
c0cddf <c0cddf@0f01eb16-6d9f-4518-cd9d-0c10718a35a1>
2013-12-25 23:15:27 +0000

    -

--- trunk/Kashkina Anna/Task 3/s03e08.cpp
+++ trunk/Kashkina Anna/Task 3/s03e08.cpp
@@ -1,12 +1,91 @@
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
 using namespace std;
 
 //Binary tree : on-line deleting the element
 
-const int maxn = 200000;
+struct Btree;
+struct Dtree;
+void merge(Dtree*&, Dtree*, Dtree*);
+pair<Dtree*, Dtree*> split(Dtree*, int);
+
+struct Dtree {
+        Dtree* l, *r;
+        int val, dat;
+        int y;
+        Dtree() {}
+        Dtree(int val, int dat) : dat(dat), val(val), y(rand() * RAND_MAX + rand()), l(0), r(0) {}
+};
+
+Dtree* add(Dtree*&, int, int);
+
+bool check(Dtree* t, int x) {
+        if (t->val == x) return 1;
+        if (t->val < x) return (t->r ? check(t->r, x) : 0);
+        return (t->l ? check(t->l, x) : 0);
+}
+
+int& get(Dtree* t, int x) {
+        if (t->val == x) return t->dat;
+        if (t->val < x) return get(t->r, x);
+        return get(t->l, x);
+}
+
+Dtree* add(Dtree*& t, int x, int dat) {
+        pair<Dtree*, Dtree*> a = split(t, x);
+        Dtree* res = new Dtree(x, dat);
+        merge(t, a.first, res);
+        merge(t, t, a.second);
+        return res;
+}
+
+void merge(Dtree* &t, Dtree* l, Dtree* r) {
+        if (!l || !r) {
+                t = (l ? l : r);
+        } else if (l->y > r->y) {
+                merge(l->r, l->r, r);
+                t = l;
+        } else {
+                merge(r->l, l, r->l);
+                t = r;
+        }
+}
+
+pair<Dtree*, Dtree*> split(Dtree* t, int x) {
+        if (!t) return make_pair(t, t);
+        if (t->val < x) {
+                pair<Dtree*, Dtree*> res = split(t->r, x);
+                t->r = res.first;
+                res.first = t;
+                return res;
+        } else {
+                pair<Dtree*, Dtree*> res = split(t->l, x);
+                t->l = res.second;
+                res.second = t;
+                return res;
+        }
+}
+
+void dfs(Dtree* t) {
+        if (!t) return;
+        dfs(t->l);
+        cout << "#" << t->val << ": " << t->dat << " ";
+        dfs(t->r);
+}
+
+struct myArray {
+        Dtree* head;
+        myArray() { head = new Dtree(-1, -1); }
+        public: int& operator[] (const int& i) {
+                if (!check(head, i)) { add(head, i, 0); }
+                return get(head, i);
+        }
+};
+
 struct Btree {
-        int p[maxn], l[maxn], r[maxn], val[maxn], sum[maxn];
+        myArray p, l, r, val, sum;
+        Btree() : p(myArray()), r(myArray()), val(myArray()), sum(myArray()) {}
         void upd(int id) {
                 sum[id] = sum[l[id]] + sum[r[id]];
         }
