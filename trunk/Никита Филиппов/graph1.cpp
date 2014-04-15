bd8dc9d15008ea362ec721f4572d329b44d90420
nikitanik387@gmail.com <nikitanik387@gmail.com@0f01eb16-6d9f-4518-cd9d-0c10718a35a1>
2014-04-15 18:28:05 +0000

    gamiltonian

---
+++ trunk/Никита Филиппов/graph1.cpp
@@ -0,0 +1,43 @@
+// graph1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+	int mas[5][5]=
+	{/*таблица смежности 5*5  */
+		{0,0,1,0,1},
+		{0,0,1,1,1},
+		{1,1,0,0,0},
+		{0,1,0,0,1},
+		{1,1,0,1,0},
+	};
+	bool visited[5]={false,false,false,false,false};
+	
+bool gamiltonian (int v,int start,int length)
+{
+	if ((length==1) && (mas[v][start]==1)) return true;
+	visited[v]=true;
+	for (int i=0;i<5;i++)
+	{
+		if (mas[i][v]=1) 
+		{
+			if (visited[i]==false)
+			{
+				if (gamiltonian(i,start,length-1)==true) return true;
+			}
+			
+		}
+	}
+	visited[v]=false;
+	return false;
+}
+int main()
+{
+	if (gamiltonian(0,0,5)) cout<<"Гамильтонов цикл есть"; else cout<<"Гамильтонова цикла нет";
+
+}
+	
+
+	
+
