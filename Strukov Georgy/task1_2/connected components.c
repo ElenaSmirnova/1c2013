8ed8eee45be7701bfff4f9a0de5a351543491e34
5napalmman@gmail.com <5napalmman@gmail.com@0f01eb16-6d9f-4518-cd9d-0c10718a35a1>
2014-04-23 20:15:55 +0000

    connected components

---
+++ Strukov Georgy/task1_2/connected components.c
@@ -0,0 +1,44 @@
+// 11.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <stdio.h>
+#define maxN 1000
+
+bool edge[maxN][maxN];
+bool visited[maxN];
+int n,m,components;
+
+void dfs(int v) {
+        visited[v]=1;
+        for(int w=0;w<n;w++) {
+                if(!visited[w] && edge[v][w]) {
+                        dfs(w);
+                }
+    }
+}
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+        printf("Enter number of vertexes: ");
+        scanf("%d",&n);
+        printf("Enter number of edges: ");
+        scanf("%d",&m);
+        printf("Enter edges: ");
+        for(int i=0;i<m;i++) {
+            int a,b;
+            scanf("%d %d",&a,&b);
+            edge[--a][--b]=edge[b][a]=1;
+        }      
+        for(int i=0;i<n;i++) {
+                if(!visited[i]) {
+                        dfs(i);
+                        components++;
+                }
+        }
+        printf("There is %d components\n",components);
+        scanf("\n");
+        return 0;
+}
